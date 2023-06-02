f25ed93fa8cc5de8c3deb07eab5f5f45d10301f9
Gyinae <dev.gyinae@gmail.com>
2023-06-02 19:04:15 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import ReactDom from "react-dom";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<App />);
+
+// ReactDom.render(<App />, document.getElementById("root"));
